db85a11a089ee9f92d83497462c88e9da4cc3c1b
Jerry Tumsfeld <truck@notonfire.co.uk>
2013-04-13 23:43:45 +0300

    copy initial copper to alloc'd chipram; make 'bars'
    : the bars don't work yet - debug!

--- demosrc/makebars.s
+++ demosrc/makebars.s
@@ -1,1 +1,40 @@
-add the makebars file (which makes copperbars)
+; vim: ft=asm68k
+; still hate vim
+; routine to make the copperbars.
+; a1 contains our spottage.
+; we start on line 30 ($1E)
+
+* exports
+    xdef    makebars
+
+makebars:
+
+    move.l  #$1E,d1
+loop1:
+;color blue
+    move.l  #$01800F08,(a1)+
+    rts
+; wait for $40 position horizontally
+    move.w  d1,d0
+    or.w   #%1000000000000000,d0
+    add.b   #$40,d0
+    move.w  d0,(a1)+
+    move.w  #$FF00,(a1)+
+; color white
+    move.l  #$01800FFF,(a1)+
+; wait for $A0 horizontally
+    add.w   #$60,d0
+    move.w  d0,(a1)+
+    move.w  #$FF00,(a1)+
+;color green
+    move.l  #$01800080,(a1)+
+; wait for end of line (or really far)
+    add.w   #$5F,d0
+    move.w  d0,(a1)+
+    move.w  #$FF00,(a1)+
+; inc and check
+    addq    #1,d1
+    cmp.w   #$0118,d1
+    blt.s   loop1
+    rts
+
--- demosrc/start.s
+++ demosrc/start.s
@@ -1,3 +1,5 @@
+; vim: ft=asm68k
+* I HATE VIM. I'm using my VISUX plugin.
 *    ______.
 *   /  /   |
 * _/  /|   l______
@@ -15,6 +17,12 @@
     include 'exec/exec_lib.i'
     include 'exec/memory.i'
 
+*** imports
+    xref makebars
+
+*** exports
+; use xdef
+
 *** Macros
 waitblit:       macro
 wb\@:
@@ -31,7 +39,7 @@ startlist=38
 
 * --- Program Start
 
-accprogstart:
+bkprogstart:
     move.l  execbase,a6
     jsr     _LVOForbid(a6)
 init:   move.l  $dff004,d0 ; vposr
@@ -57,14 +65,26 @@ init:   move.l  $dff004,d0 ; vposr
 
 ; allocate 9200 bytes for copperlist
     move.l  #9200,d0
-    move.l  (MEMF_CHIP|MEMF_CLEAR),d1
+    move.l  #MEMF_CHIP!MEMF_CLEAR,d1
     jsr     _LVOAllocMem(a6)
     move.l  d0,coppa
     beq exit1
 
+; copy the block to start
+    move.l d0,a1
+    move.l #copper1,a0
+    move.b #((copperend-copper1)/4)-1,d1
+.lo1
+    move.l (a0)+,(a1)+
+    dbra    d1,.lo1
+; entering the routine A1 points to the next bit.
+    jsr makebars
+    move.l #$FFFFFFFE,(a1)+
+;
+
 * copper init
     lea     $dff000,a0
-    move.l  #copper1,cop1lc(a0)
+    move.l  coppa,cop1lc(a0)
     move.w  copjmp1(a0),d0
 
 * --- Main Body of Demo
@@ -109,11 +129,7 @@ grname: dc.b    "graphics.library",0,0
 screenmem:
     dc.l 0,0,0,0,0,0
 coppa:
-       dc.l 0  
-;screen2:
-;       dc.l $70000,$73000
-;xposmat:
-;       dcb.l 1580,0    
+    dc.l 0  
 
     even
 ; - Includes for various things (code) like replays
@@ -133,7 +149,7 @@ copper1:
 ;
 ; Load color registers
 ;
-     DC.W    $180,$0FFF      ;Move white into register $180 (COLOR00
+     DC.W    $180,$0FFF      ;Move white into register $180 (COLOR00)
      DC.W    $182,$0F00      ;Move red into register $182 (COLOR01)
      DC.W    $184,$00F0      ;Move green into register $184 (COLOR02)
      DC.W    $186,$000F      ;Move blue into register $186 (COLOR03)
@@ -144,18 +160,20 @@ copper1:
 ;
 ; Wait for line 150
 ;
-     DC.W    $9601,$FF00    ;Wait for line 150, ignore horiz. position
+     DC.W    $1E01,$FF00    ;Wait for line 30, ignore horiz. position
 ;
 ; Change color registers mid-display
 ;
-     DC.W    color+00,$0000      ;Move black into register $0180 (COLOR00)
-     DC.W    color+02,$0FF0      ;Move yellow into register $0182 (COLOR01)
-     DC.W    color+04,$00FF      ;Move cyan into register $0184 (COLOR02)
-     DC.W    color+06,$0F0F      ;Move magenta into register $0186 (COLOR03)
+;     DC.W    color+00,$0000      ;Move black into register $0180 (COLOR00)
+;     DC.W    color+02,$0FF0      ;Move yellow into register $0182 (COLOR01)
+;     DC.W    color+04,$00FF      ;Move cyan into register $0184 (COLOR02)
+;     DC.W    color+06,$0F0F      ;Move magenta into register $0186 (COLOR03)
 ;
 ;  End Copper list by waiting for the impossible
 ;
-     DC.W    $FFFF,$FFFE    ;Wait for line 255, H = 254 (never happens)
+;     DC.W    $FFFF,$FFFE    ;Wait for line 255, H = 254 (never happens)
+
+copperend
 
 * - music data
 
