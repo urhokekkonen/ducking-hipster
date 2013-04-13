a19733218b5a4c5b29baa6c6747ec5bef71d94ac
Jerry Tumsfeld <truck@notonfire.co.uk>
2013-04-13 18:37:26 +0300

    memory allocation for copperlist. (currently nothing else. Futzing with environment.) (does compile, dunno why MEMF_CHIP didn't work for the alloc but MEMF_CHIP|MEMF_CLEAR does.

--- demosrc/start.s
+++ demosrc/start.s
@@ -1,38 +1,30 @@
-;APS00000000000000000000000000000000000000000000000000000000000000000000000000000000
-*      ____________________
-*   /\/  ) )_) _) _)_)\    ) A500 demosystem
-*  /  \ ( ( _)_ )_ )|  )  /  V .001
-* (__)_)_)_)_)_/__/_|_/__/   2008 Sir Garbagetruck
+*    ______.
+*   /  /   |
+* _/  /|   l______
+* |  /_|         / BREaKFAST KLUB
+* |  .  \     __/_  Demo "System"
+* |     /         \
+* |____/   \_______\
+*      |_____|
+*      
+*  Demo: "en tiedä"
 
-* Thanks to Evil for giving me some help with the lame 'restart'
-* issues, like how I can never see the freaking AsmOne/AsmPro
-* 'label:' thing correctly; I always think it needs to be on
-* in order to use labels that end with :, and I have no idea
-* what it does other than fuck stuff up with C= Includes.
-* The initial 1.3 startup stuff was from him but it's been redone
-* modernized and truckified.
-
-* Edit was done on osX using TextMate and my m68k colorizing bits.
-* followed by just doing it in asmpro.
-
-*** includes 
-	incdir	/opt/wincrap/winuae/werk/entieda/include/
-;	include	'truckincludes.i'
-	include 'hardware/custom.i'
-	include 'exec/exec_lib.i'
-;	include 'hardware/othercustom.i'
+*** includes
+    incdir  work:include
+    include 'hardware/custom.i'
+    include 'exec/exec_lib.i'
+    include 'exec/memory.i'
 
 *** Macros
-waitblit:	macro
+waitblit:       macro
 wb\@:
-	btst #$0e,$dff002
-	bne.s wb\@
-	endm
-
-	
+    btst #$0e,$dff000
+    bne.s wb\@
+    endm
+    
 * - Program portion
-	section	program,code
-	
+    section BKprogram,code
+    
 *** constants
 execbase=4
 startlist=38
@@ -40,17 +32,17 @@ startlist=38
 * --- Program Start
 
 accprogstart:
-	move.l	execbase,a6
-	jsr	_LVOForbid(a6)
-init:	move.l	$dff004,d0 ; vposr
-	and.l	#$0001ff00,d0
-	cmp.l	#$00001000,d0
-	bne.s	init
-	move.w	#%0000000000100000,$dff096 ; dmacon
+    move.l  execbase,a6
+    jsr     _LVOForbid(a6)
+init:   move.l  $dff004,d0 ; vposr
+    and.l   #$0001ff00,d0
+    cmp.l   #$00001000,d0
+    bne.s   init
+    move.w  #%0000000000100000,$dff096 ; dmacon
 
 * - music init, other inits before we get going
 
-;	jsr	mt_init
+;       jsr     mt_init
 
 ; alloc mem for bitplanes
 ; 32000 bytes chipmem each
@@ -60,64 +52,75 @@ init:	move.l	$dff004,d0 ; vposr
     sub.l   d1,d1   ;don't care where in mem
     jsr     _LVOAllocMem(a6)
     move.l  d0,hunkaram
-;   errorcheck, put in if we go over 4k
-;    beq exit
+;   errorcheck -if 0 we have no mem
+    beq exit0
 
-; ok you need to unallocate, don't compile this without it
-	
+; allocate 9200 bytes for copperlist
+    move.l  #9200,d0
+    move.l  (MEMF_CHIP|MEMF_CLEAR),d1
+    jsr     _LVOAllocMem(a6)
+    move.l  d0,coppa
+    beq exit1
 
 * copper init
-	lea	$dff000,a0
-	move.l	#copper1,cop1lc(a0)
-	move.w	copjmp1(a0),d0
+    lea     $dff000,a0
+    move.l  #copper1,cop1lc(a0)
+    move.w  copjmp1(a0),d0
 
 * --- Main Body of Demo
-wait:	move.l	$dff004,d0 ; vposr
-	and.l	#$0001ff00,d0
-	cmp.l	#$00001000,d0
-	bne.s	wait
+wait:   move.l  $dff004,d0 ; vposr
+    and.l   #$0001ff00,d0
+    cmp.l   #$00001000,d0
+    bne.s   wait
 
-	btst	#6,$bfe001
-	bne.s	wait
+    btst    #6,$bfe001
+    bne.s   wait
 
 * -- End Program
-fin:	move.l	execbase,a6
-	move.l	#grname,a1
-	clr.l	d0
-	jsr	_LVOOpenLibrary(a6)
-	move.l	d0,a4
-	move.l	startlist(a4),$dff080 ; cop1lch
-	clr.w	$dff088 ; copjmp1
+fin:    move.l  execbase,a6
+    move.l  #grname,a1
+    clr.l   d0
+    jsr     _LVOOpenLibrary(a6)
+    move.l  d0,a4
+    move.l  startlist(a4),$dff080 ; cop1lch
+    clr.w   $dff088 ; copjmp1
 ; free our ... stuff
-	move.l hunkaram,a1
-    	move.l #1000,d0
-    	jsr _LVOFreeMem(a6)
+exit2:
+    move.l coppa,a1
+    move.l #1000,d0
+    jsr _LVOFreeMem(a6)
 
-	move.w	#%1000000000100000,$dff096 ; dmacon
-;	jsr	mt_end
-	jsr	_LVOPermit(a6)
-	clr.l	d0
-	rts
+exit1:
+    move.l hunkaram,a1
+    move.l #1000,d0
+    jsr _LVOFreeMem(a6)
 
-grname:	dc.b	"graphics.library",0,0
+exit0:
+    move.w  #%1000000000100000,$dff096 ; dmacon
+;       jsr     mt_end
+    jsr     _LVOPermit(a6)
+    clr.l   d0
+    rts
 
-	even
+grname: dc.b    "graphics.library",0,0
+
+    even
 * locations for 6 bitplanes
 screenmem:
-	dc.l 0,0,0,0,0,0
-;stackbackup:
-;	dc.l 0	
+    dc.l 0,0,0,0,0,0
+coppa:
+       dc.l 0  
 ;screen2:
-;	dc.l $70000,$73000
+;       dc.l $70000,$73000
 ;xposmat:
-;	dcb.l 1580,0	
+;       dcb.l 1580,0    
 
-	even
+    even
 ; - Includes for various things (code) like replays
-;	include	fastreplay.s
+;       include fastreplay.s
 
 * Chip stuff, in another section
-	section	copper-image,data_c
+    section copper-image,data_c
 
 copper1:
 ;
@@ -141,7 +144,7 @@ copper1:
 ;
 ; Wait for line 150
 ;
-     DC.W    $9601,$FF00        ;Wait for line 150, ignore horiz. position
+     DC.W    $9601,$FF00    ;Wait for line 150, ignore horiz. position
 ;
 ; Change color registers mid-display
 ;
@@ -152,14 +155,14 @@ copper1:
 ;
 ;  End Copper list by waiting for the impossible
 ;
-     DC.W    $FFFF,$FFFE        ;Wait for line 255, H = 254 (never happens)
+     DC.W    $FFFF,$FFFE    ;Wait for line 255, H = 254 (never happens)
 
 * - music data
 
-;mt_data:	incbin	mod.musique
+;mt_data:       incbin  mod.musique
 
-hunkaram:	dc.l	0,0
+hunkaram:       dc.l    0,0
 
 * bitplanes
-;plane1	blk.b	32000
+;plane1 blk.b   32000
 
